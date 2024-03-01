243417638c06c1a306e5272ab6403ee81878b3f0
Eliza Weisman <eliza@elizas.website>
2024-03-01 10:33:12 -0800

    put back servertest.rs

--- src/bin/servertest.rs
+++ src/bin/servertest.rs
@@ -1,22 +1,19 @@
-// use std::io::Read;
+use std::io::Read;
 
-// fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-//     let mut text = String::new();
-//     std::io::stdin().read_to_string(&mut text)?;
+fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let mut text = String::new();
+    std::io::stdin().read_to_string(&mut text)?;
 
-//     let iface: idol::syntax::Interface = ron::de::from_str(&text)?;
-//     let mut out = std::io::stdout();
+    let iface: idol::syntax::Interface = ron::de::from_str(&text)?;
 
-//     idol::server::generate_server_constants(&iface, &mut out)?;
-//     idol::server::generate_server_conversions(&iface, &mut out)?;
-//     idol::common::generate_op_enum(&iface, &mut out)?;
-//     idol::server::generate_server_op_impl(&iface, &mut out)?;
+    let tokens = idol::server::generate_restricted_server_support(
+        &iface,
+        idol::server::ServerStyle::InOrder,
+        &Default::default(),
+    )?;
+    let syntax_tree = syn::parse2::<syn::File>(tokens)?;
+    let formatted = prettyplease::unparse(&syntax_tree);
+    println!("{formatted}");
 
-//     idol::server::generate_server_in_order_trait(
-//         &iface,
-//         &mut out,
-//         &Default::default(),
-//     )?;
-
-//     Ok(())
-// }
+    Ok(())
+}
