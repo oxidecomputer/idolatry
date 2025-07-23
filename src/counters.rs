8075963e08aa8fed53cc501961c0265e14503f71
Eliza Weisman <eliza@elizas.website>
2025-07-23 09:46:59 -0700

    Actually...just use the macro
    There's no reason not to just use the `counters::counters!` macro for
    IPC counters,rather than generating the same code in idol codegen. This
    way, we're always in sync
    with what other uses of the `counters` crate get. If we had been doing
    this all along, we would have just gotten the `#[used]` change from
    oxidecomputer/hubris#2167 without having to make changes in idol.

--- src/counters.rs
+++ src/counters.rs
@@ -172,8 +172,7 @@ impl Counters {
         quote! {
             #enum_def
 
-            static #counters_static: <#event_enum as counters::Count>::Counters =
-                <#event_enum as counters::Count>::NEW_COUNTERS;
+            counters::counters!(#counters_static, #event_enum);
         }
     }
 
